b3f650b1485959a41eac91f77540b4e9600a6a74
Andrew Reifman <andrewr23@gmail.com>
2014-12-20 16:36:57 -0500

    added audio files

--- js/main.js
+++ js/main.js
@@ -6,31 +6,44 @@ $(document).ready(function() {
 
   $(".part-2 h1").lettering()
 
-  setRandomClass();
+  var itemList = [0,1,2,3,4]
+
+  setRandomClass(itemList);
   $('.happy > div').click(function(){
     if ($(this).hasClass('selected')) {
-      setRandomClass();
-      $('.happy img').removeClass('tada');
+      setRandomClass(itemList);
       $(this).find('img').addClass('tada');
+      setTimeout(function(){
+        $('.happy img').removeClass('tada');
+      }, 500);
+
+      var number = $('.bells audio.selected').index();
+      $('.bells audio').get(number).play();
     }
   });
 
-  function setRandomClass() {
-    var parent = $(".happy");
-    var items = parent.find("> div");
-    var number = items.length;
-    var random = Math.floor((Math.random() * number));
+  function setRandomClass(itemList) {
+    var items = $(".happy").find("> div");
+    var random = Math.floor(Math.random() * itemList.length);
     items.removeClass("selected");
-    items.eq(random).addClass("selected");
+    setTimeout(function(){
+      items.eq(random).addClass("selected");
+    }, 300);
   }
 
-  $('.holidays').click(function(){
-    $('.part-1').addClass('bounceOutUp');
-    $('#bgvid').get(0).play();
+  $(".bells audio").on("ended", function(){
+    var selectedBell = $(this);
+    if (selectedBell.is(':last-child')) {
+      $('.part-1').addClass('bounceOutUp');
+      $('.song').get(0).play();
+      $('#bgvid').get(0).play();
 
-    setTimeout(function(){
-      $('.part-2').addClass('active');
-      $(".part-2 h1").animateLetters({top:-40,opacity:0},{top:0,opacity:1},{randomOrder:false,time:600,reset:true});
-    }, 2000);
+      setTimeout(function(){
+        $('.part-2').addClass('active');
+        $(".part-2 h1").animateLetters({top:-40,opacity:0},{top:0,opacity:1},{randomOrder:false,time:600,reset:true});
+      }, 2000);
+    } else {
+      selectedBell.removeClass().next("audio").addClass('selected')
+    }
   });
 });
